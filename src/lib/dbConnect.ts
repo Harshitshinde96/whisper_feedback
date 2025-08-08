e444a778e008da238f8cfbda87e6230d4f3ebb76
=Harshitshinde96 <=harshitshinde65@gmail.com>
2025-08-09 00:32:49 +0530

    Next js Continue

---
+++ src/lib/dbConnect.ts
@@ -0,0 +1,28 @@
+import mongoose, { Connection } from "mongoose";
+
+type ConnectionObject = {
+  isConnected?: number;
+};
+
+const connection: ConnectionObject = {};
+
+async function dbConnect(): Promise<void> {
+  // Void means i dont care which type of data returns
+  if (connection.isConnected) {
+    console.log("Alredy Connected to database");
+    return;
+  }
+
+  try {
+    const db = await mongoose.connect(process.env.MONGODB_URI || "", {});
+
+    connection.isConnected = db.connections[0].readyState;
+
+    console.log("DB Connected Successfully");
+  } catch (error) {
+    console.log("Database connection failed", error);
+    process.exit(1);
+  }
+}
+
+export default dbConnect;
---
+++ src/schemas/signUpSchema.ts
@@ -0,0 +1,18 @@
+import { z } from "zod";
+
+export const usernameValidation = z
+  .string()
+  .min(3, "Username must be atleast 3 characters")
+  .max(20, "Username must not be more than 20 characters")
+  .regex(
+    /^[a-zA-Z0-9_-]{3,20}$/,
+    "Username must not contain special character"
+  );
+
+export const signUpSchema = z.object({
+  username: usernameValidation,
+  email: z.string().email({ message: "Invalid email address" }),
+  password: z
+    .string()
+    .min(6, { message: "Password must atleast 6 characters" }),
+});
