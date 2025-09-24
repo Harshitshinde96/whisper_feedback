63fc538956a88bc0fa9e6284fc29b6a04331a5a1
=Harshitshinde96 <=harshitshinde65@gmail.com>
2025-09-24 13:42:56 +0530

    Email Domain Error Solving attempt 1

--- src/lib/resend.ts
+++ src/lib/resend.ts
@@ -1,4 +1,6 @@
 // lib/resend.ts
 import { Resend } from "resend";
 
+// console.log("Resend API Key present?", !!process.env.RESEND_API_KEY);
+
 export const resend = new Resend(process.env.RESEND_API_KEY!);
